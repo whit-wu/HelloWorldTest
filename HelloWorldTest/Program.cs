ec9f8c6ff5900ccb335b166ce08e0133f30bfcfc
Whit Wu <whit.wu@gmail.com>
2022-08-21 13:21:57 -0400

    Update Program.cs

--- HelloWorldTest/Program.cs
+++ HelloWorldTest/Program.cs
@@ -1,13 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
-public class Hello { 
-
-    public string HelloThere()
-    {
-        // replace this with a string to return.
-        throw new Exception();
-    }
-
-}
